966de06c23795cd6fd67dcc112ef4f498b506780
Bruce <zmzralph@gmail.com>
2025-09-22 13:39:26 +0000

    add command return file_name

--- src/langbot_plugin/api/entities/builtin/command/context.py
+++ src/langbot_plugin/api/entities/builtin/command/context.py
@@ -28,6 +28,10 @@ class CommandReturn(pydantic.BaseModel):
     """文件链接
     """
 
+    file_name: typing.Optional[str] = None
+    """文件名称
+    """
+
     error: typing.Optional[errors.CommandError] = None
     """错误，保留供系统使用，插件逻辑报错请自行使用 text 传递
     """
