09a20e9acbbffad79206ebb1273331e82ae410f5
Junyan Qin <rockchinq@gmail.com>
2025-11-27 17:44:12 +0800

    perf: unify retrieval result entry result content structure

--- src/langbot_plugin/api/entities/builtin/rag/context.py
+++ src/langbot_plugin/api/entities/builtin/rag/context.py
@@ -3,9 +3,13 @@ from __future__ import annotations
 import pydantic
 from typing import Any
 
+from langbot_plugin.api.entities.builtin.provider.message import ContentElement
+
 
 class RetrievalResultEntry(pydantic.BaseModel):
     id: str
+
+    content: list[ContentElement]
 
     metadata: dict[str, Any]
 
@@ -18,8 +22,5 @@ class RetrievalContext(pydantic.BaseModel):
     query: str
     """The query."""
 
-    top_k: int
+    top_k: int = pydantic.Field(default=5)
     """The top k."""
-
-    result: list[RetrievalResultEntry]
-    """The retrieval result."""
